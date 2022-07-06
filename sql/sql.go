b16dfcb2d0da1a7a7aaa9e44d904423160ce68cb
MuhamadSholikhudin <muhammadsholihudin18@gmail.com>
2022-07-06 16:01:28 +0700

    06 07 2022 16:00

---
+++ sql/sql.go
@@ -0,0 +1,90 @@
+
+package main
+
+import "fmt"
+import "database/sql"
+import _ "github.com/go-sql-driver/mysql"
+
+type student struct {
+    id    int
+    name  string
+    number_of_employees   string
+}
+
+func main() {
+    sqlQuery()
+    // sqlQueryRow()
+}
+
+func connect() (*sql.DB, error) {
+    db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/hrdit")
+    if err != nil {
+        return nil, err
+    }
+
+    return db, nil
+}
+
+
+func sqlQuery() {
+    db, err := connect()
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+    defer db.Close()
+
+    var age = 0
+    rows, err := db.Query("select id, name, number_of_employees from employees where id > ?", age)
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+    defer rows.Close()
+
+    var result []student
+
+    for rows.Next() {
+        var each = student{}
+        var err = rows.Scan(&each.id, &each.name, &each.number_of_employees)
+
+        if err != nil {
+            fmt.Println(err.Error())
+            return
+        }
+
+        result = append(result, each)
+    }
+
+    if err = rows.Err(); err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+
+    for _, each := range result {
+        fmt.Println(each.name)
+    }
+}
+
+
+func sqlQueryRow() {
+    var db, err = connect()
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+    defer db.Close()
+
+    var result = student{}
+    var id = "E001"
+    err = db.
+        QueryRow("select name, grade from tb_student where id = ?", id).
+        Scan(&result.name, &result.grade)
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+
+    fmt.Printf("name: %s\ngrade: %d\n", result.name, result.grade)
+}
+
